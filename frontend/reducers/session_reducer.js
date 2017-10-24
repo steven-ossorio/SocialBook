447abdc427ecb7368103357fbfb1058f2f5d07d6
Steven <codebusiness2014@gmail.com>
2017-10-24 18:01:22 -0400

    Created reducers for session and session errors

---
+++ frontend/reducers/session_reducer.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import merge from 'lodash';
+
+import { RECEIVE_CURRENT_USER } from '../actions/session_actions';
+
+const originalState = {
+  currentUser: null
+};
+
+const SessionReducer = (state = originalState, action) => {
+  Object.freeze(state);
+  let nextState;
+  switch (action.type) {
+    case RECEIVE_CURRENT_USER:
+      nextState = merge({}, state);
+      nextState.currentUser = action.currentUser;
+      return nextState;
+    default:
+      return state;
+  }
+};
+
+export default SessionReducer;
