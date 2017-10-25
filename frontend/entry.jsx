88eef64055c7d3e8eb4358c0ecce0e0906573f5c
Steven <codebusiness2014@gmail.com>
2017-10-25 09:22:40 -0400

    Making sure store is configured correctly

--- frontend/entry.jsx
+++ frontend/entry.jsx
@@ -1,8 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { login, logout, signup } from './util/session_api_util';
+import configureStore from './store/store';
 
 document.addEventListener('DOMContentLoaded', () => {
+  const store = configureStore();
+  window.getState = store.getState;
+  window.dispatch = store.dispatch;
   window.login = login;
   window.logout = logout;
   window.signup = signup;
