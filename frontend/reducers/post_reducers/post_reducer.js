09c5e667ccae00a600b203257338d8a95457c1b5
Steven <codebusiness2014@gmail.com>
2017-10-27 19:31:52 -0400

    Added a post reducer to recieve response on success

--- frontend/reducers/post_reducers/post_reducer.js
+++ frontend/reducers/post_reducers/post_reducer.js
@@ -1,2 +1,27 @@
 import React from 'react';
 import merge from 'lodash';
+
+import { RECEIVE_ALL_POSTS, RECEIVE_POST, REMOVE_POST } from '../../actions/post_actions';
+
+const originalState = {
+  text: ""
+};
+
+const PostReducer = (state = originalState, action) => {
+  let newState;
+  Object.freeze(state);
+  switch (action.type) {
+    case RECEIVE_ALL_POSTS:
+      return action.posts;
+    case RECEIVE_POST:
+      return merge({}, state, { [action.post.id]: action.post });
+    case REMOVE_POST:
+      nextState = merge({}, state);
+      delete nextState[action.postId];
+      return nextState;
+    default:
+      return state;
+  }
+};
+
+export default PostReducer;
