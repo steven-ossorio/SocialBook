d47783d7a986cdd20ace47a777d8a52b645227ce
Steven <codebusiness2014@gmail.com>
2017-10-27 19:35:30 -0400

    Added post reducer to the root reducer

--- frontend/reducers/root_reducer.js
+++ frontend/reducers/root_reducer.js
@@ -1,10 +1,12 @@
 import { combineReducers } from 'redux';
 import SessionReducer from './session_reducers/session_reducer';
+import PostReducer from './post_reducers/post_reducer';
 import ErrorsReducer from './errors_reducer';
 
 const RootReducer = combineReducers({
   session: SessionReducer,
-  errors: ErrorsReducer
+  errors: ErrorsReducer,
+  post: PostReducer
 });
 
 export default RootReducer;
