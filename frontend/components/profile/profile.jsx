0fc6f27d80f7ff1fc4021cf20c35216a9be02977
Steven <codebusiness2014@gmail.com>
2017-10-26 20:57:39 -0400

    working on profile container props

---
+++ frontend/components/profile/profile.jsx
@@ -0,0 +1,45 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+
+class Profile extends Component {
+  constructor(props){
+    super(props);
+
+  }
+
+  render(){
+    console.log(this.props.currentUser);
+    if (this.props.currentUser) {
+      return(
+        <div>
+          <main className="newsfeed-nav-container">
+            <div className="newsfeed-nav-items">
+              <h1 className="logo"><Link to="/"><i className="fa fa-facebook-official"></i></Link></h1>
+              <ul className="navbar-items">
+                <li>
+                  <img className="nav-profile-image" src={ this.props.currentUser.image_url }></img>
+                  <Link to={`/users/${ this.props.currentUser.id}` }>{ this.props.currentUser.firstName }</Link>
+                </li>
+                <li>Home</li>
+                <li><i className="fa fa-users"></i></li>
+                <li><i className="fa fa-comments"></i></li>
+                <li><i className="fa fa-globe"></i></li>
+                <li><i className="fa fa-question-circle"></i></li>
+                <li><i className="fa fa-arrow-circle-down"></i></li>
+              </ul>
+            </div>
+          </main>
+          { this.props.currentUser.image_url }
+          <button onClick={ this.props.logout }>Logout</button>
+        </div>
+      );
+    } else {
+      return (
+        <div>
+        </div>
+      );
+    }
+  }
+}
+
+export default Profile;
