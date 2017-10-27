d07005b21f23f0febcc6d5f66094d632b971daaa
Steven <codebusiness2014@gmail.com>
2017-10-27 19:35:14 -0400

    Added post errors to the collection of errors

--- frontend/reducers/errors_reducer.js
+++ frontend/reducers/errors_reducer.js
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux';
 import SessionErrorsReducer from './session_reducers/session_errors_reducer';
+import PostErrorsReducer from './post_reducers/post_errors_reducer';
 
 const ErrorsReducer = combineReducers({
-  session: SessionErrorsReducer
+  session: SessionErrorsReducer,
+  post: PostErrorsReducer
 });
 
 export default ErrorsReducer;
