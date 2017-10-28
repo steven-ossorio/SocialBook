21de2661efe6d08b74fab0667df786ee1cd51da4
Steven <codebusiness2014@gmail.com>
2017-10-28 16:54:21 -0400

    Moved each part into a testing section. To be moved once feature has been fully completed

--- frontend/components/app.jsx
+++ frontend/components/app.jsx
@@ -2,6 +2,8 @@ import React from 'react';
 import HomeContainer from './home/home_container';
 import SessionFormContainer from './session/session_form_container';
 import SignUpFormContainer from './signup/signup_form_container';
+import PostFormContainer from './post/post_form_container';
+import PostIndexContainer from './post/post_index_container';
 import ProfileContainer from './profile/profile_container';
 import { Route, Link, Redirect, Switch } from 'react-router-dom';
 import { AuthRoute, ProtectedRoute } from '../util/route_util';
@@ -10,6 +12,8 @@ import { AuthRoute, ProtectedRoute } from '../util/route_util';
 const App = () => (
     <div>
       <header>
+        <Route exact path='/testing' component={PostFormContainer} />
+        <Route exact path='/testing' component={PostIndexContainer} />
         <Route exact path='/' component={HomeContainer}/>
         <Route path="/users/:userId" component={ProfileContainer}/>
       </header>
