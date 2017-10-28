7c0412b30929ecc61fa36c3bdbc6c54751f7c084
Steven <codebusiness2014@gmail.com>
2017-10-27 20:07:41 -0400

    Need to add a console.log for state

--- frontend/components/session/session_form_container.jsx
+++ frontend/components/session/session_form_container.jsx
@@ -3,6 +3,7 @@ import { connect } from  'react-redux';
 import SessionForm from './session_form';
 
 const mapStateToProps = state => {
+    console.log(state);
     return {
       loggedIn: Boolean(state.session.currentUser),
       errors: state.errors.session
