41a35780878347ece77825ae8e3e37fd10120bb5
Steven <codebusiness2014@gmail.com>
2017-10-30 12:08:37 -0400

    Imported user error reducer

--- frontend/reducers/errors_reducer.js
+++ frontend/reducers/errors_reducer.js
@@ -1,10 +1,12 @@
 import { combineReducers } from 'redux';
 import SessionErrorsReducer from './session_reducers/session_errors_reducer';
 import PostErrorsReducer from './post_reducers/post_errors_reducer';
+import UserErrorsReducer from './user_reducers/user_errors_reducer';
 
 const ErrorsReducer = combineReducers({
   session: SessionErrorsReducer,
-  post: PostErrorsReducer
+  post: PostErrorsReducer,
+  user: UserErrorsReducer
 });
 
 export default ErrorsReducer;
