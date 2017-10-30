8a564f9c7a57d2dee044ab68e1eea51b2d79796f
Steven <codebusiness2014@gmail.com>
2017-10-30 12:08:11 -0400

    User reducer to handle incoming actions

--- frontend/reducers/user_reducers/user_reducer.js
+++ frontend/reducers/user_reducers/user_reducer.js
@@ -1,1 +1,19 @@
+import React from 'react';
+import { merge } from 'lodash';
 
+import { RECEIVE_ALL_USERS, RECEIVE_USER } from '../../actions/user_actions';
+
+const UserReducer = (state = {}, action) => {
+  let newState;
+  Object.freeze(state);
+  switch (action.type) {
+    case RECEIVE_ALL_USERS:
+      return action.users;
+    case RECEIVE_USER:
+      return merge({}, state, { [action.user.id]: action.user });
+    default:
+      return state;
+  }
+};
+
+export default UserReducer;
