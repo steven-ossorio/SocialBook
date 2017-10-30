f155849d96d601bf64c2e7ff8a7b43f358a0aba3
Steven <codebusiness2014@gmail.com>
2017-10-30 19:45:38 -0400

    Removed debugger for heroku upload. passed in proper state

--- frontend/components/post/post_index_container.jsx
+++ frontend/components/post/post_index_container.jsx
@@ -1,14 +1,14 @@
 import { fetchPosts, deletePost } from '../../actions/post_actions';
 import { fetchUser } from '../../actions/user_actions';
 import { connect } from 'react-redux';
+import { withRouter } from 'react-router-dom';
 import PostIndex from './post_index';
 
 const mapStateToProps = (state, ownProps) => {
-  console.log(state);
-  console.log(ownProps);
   return {
     posts: state.posts,
-    currentUser: state.session.currentUser
+    currentUser: state.session.currentUser,
+    user: state.users[ownProps.match.params.userId]
   };
 };
 
@@ -20,7 +20,7 @@ const mapDispatchToProps = dispatch => {
   };
 };
 
-export default connect(
+export default withRouter(connect(
   mapStateToProps,
   mapDispatchToProps
-)(PostIndex);
+)(PostIndex));
