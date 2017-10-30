08516a59b41683cbc27fb9dc1e5f2889142e778e
Steven <codebusiness2014@gmail.com>
2017-10-30 12:06:21 -0400

    Created util for user api requests

--- frontend/util/user_api_util.js
+++ frontend/util/user_api_util.js
@@ -5,17 +5,17 @@ export const fetchUsers = () => {
   });
 };
 
+export const fetchUser = id => {
+  return $.ajax({
+    method: "get",
+    url: `/api/users/${id}`
+  });
+};
+
 export const updateUser = user => {
   return $.ajax({
     method: "patch",
     url: `/api/users/${user.id}/edit`,
     data: { user }
-  });
-};
-
-export const fetchUser = id => {
-  return $.ajax({
-    method: "get",
-    url: `/api/users/${id}`
   });
 };
