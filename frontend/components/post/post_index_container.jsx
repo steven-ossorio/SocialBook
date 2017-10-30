75a08e6535b2ba410b385a057b5f5237f110235c
Steven <codebusiness2014@gmail.com>
2017-10-30 14:47:09 -0400

    Trying to refactor in order to show a user image instead of currentUser

--- frontend/components/post/post_index_container.jsx
+++ frontend/components/post/post_index_container.jsx
@@ -1,18 +1,22 @@
 import { fetchPosts, deletePost } from '../../actions/post_actions';
+import { fetchUser } from '../../actions/user_actions';
 import { connect } from 'react-redux';
 import PostIndex from './post_index';
 
-const mapStateToProps = ({ posts, session }) => {
+const mapStateToProps = (state, ownProps) => {
+  console.log(state);
+  console.log(ownProps);
   return {
-    posts,
-    currentUser: session.currentUser
+    posts: state.posts,
+    currentUser: state.session.currentUser
   };
 };
 
 const mapDispatchToProps = dispatch => {
   return {
     fetchPosts: () => dispatch(fetchPosts()),
-    deletePost: id => dispatch(deletePost(id))
+    deletePost: id => dispatch(deletePost(id)),
+    fetchUser: id => dispatch(fetchUser(id))
   };
 };
 
