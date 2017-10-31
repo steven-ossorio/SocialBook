e81389b1e595914475cf8561429c274676910781
Steven <codebusiness2014@gmail.com>
2017-10-31 15:28:28 -0400

    removed FriendReducer

--- frontend/reducers/root_reducer.js
+++ frontend/reducers/root_reducer.js
@@ -2,7 +2,6 @@ import { combineReducers } from 'redux';
 import SessionReducer from './session_reducers/session_reducer';
 import PostReducer from './post_reducers/post_reducer';
 import UserReducer from './user_reducers/user_reducer';
-import FriendReducer from './friend_reducers/friend_reducer';
 import ErrorsReducer from './errors_reducer';
 
 const RootReducer = combineReducers({
@@ -10,7 +9,6 @@ const RootReducer = combineReducers({
   errors: ErrorsReducer,
   posts: PostReducer,
   users: UserReducer,
-  friends: FriendReducer
 });
 
 export default RootReducer;
