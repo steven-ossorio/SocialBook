ed2aa912bcdc04b4da660e351ca682c8bd5ef422
Steven <codebusiness2014@gmail.com>
2017-11-01 15:22:13 -0400

    Missed passing in image parameter

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -11,7 +11,7 @@ class Api::UsersController < ApplicationController
       login(@user)
       render :show
     else
-      render json: @user.errors.messages, status: 401
+      render json: @user.errors.messages, status: 422
     end
   end
 
@@ -19,9 +19,18 @@ class Api::UsersController < ApplicationController
     @user = User.find(params[:id])
   end
 
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      render :show
+    else
+      render json: @user.errors.messages, status: 422
+    end
+  end
+
   private
 
   def user_params
-    params.require(:user).permit(:first_name, :last_name, :email, :dob, :sex, :password)
+    params.require(:user).permit(:first_name, :last_name, :email, :dob, :sex, :password, :image)
   end
 end
