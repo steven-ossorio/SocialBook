319e8a086ec7f05433dd476450da801f02b80df0
Steven <codebusiness2014@gmail.com>
2017-11-01 15:21:50 -0400

    Needed to add update for user update

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   root to: 'static_pages#root'
 
   namespace :api, defaults: { format: :json } do
-    resources :users, only: [:index, :create, :show, :edit]
+    resources :users, only: [:index, :create, :show, :update]
     resources :posts
     resources :friends, only: [:create, :edit, :update, :destroy]
     resource :session, only: [:create, :destroy]
