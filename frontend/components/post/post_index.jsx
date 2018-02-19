0ba812c94793f2c6af2db52f6ecf3e1a683f9d61
Steven <codebusiness2014@gmail.com>
2018-02-19 17:44:21 -0500

    Added a spinner for when profile post ids aren't present

--- frontend/components/post/post_index.jsx
+++ frontend/components/post/post_index.jsx
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import DropDown from './dropdown';
 import moment from 'moment';
 import { Link } from 'react-router-dom';
+import { RingLoader } from 'react-spinners';
+
 
 class PostIndex extends Component {
   constructor(props) {
@@ -30,37 +32,45 @@ class PostIndex extends Component {
   }
 
   renderPosts() {
-    let posts = this.props.profilePostsId.sort().reverse().map( id => {
-      return this.props.posts[id];
-    });
-    
-    posts = posts.filter(post=>{
-      return post;
-    });
-    return posts.map( (post, idx) => {
-
+    if (this.props.profilePostsId === null) {
       return (
-        <li className="post-list" key={ `${post.id}` }>
-          <div className="post-list-container">
-            <div className="entire-top-container">
-              <div className="post-top-container">
-                <div className="post-top-left-container">
-                  <div>
-                    <img className="post-form-image" src={ post.image }></img>
-                  </div>
-                  <div className="post-name-container">
-                    <Link to={ `/users/${post.owner}` } replace><p>{ post.first_name }</p></Link>
-                    <p>{ moment(post.created_at).format("LL").slice(0, 10) }</p>
-                  </div>
-                </div>
-                { this.postOwner(post) }
-              </div>
-            </div>
-            <p className="post-list-text">{ post.text }</p>
-          </div>
-        </li>
+        <div className="loading-spin">
+          <RingLoader size={100} color={'#0000FF'} />
+        </div>
       );
-    });
+    } else {
+      let posts = this.props.profilePostsId.sort().reverse().map( id => {
+        return this.props.posts[id];
+      });
+
+      posts = posts.filter(post=>{
+        return post;
+      });
+      return posts.map( (post, idx) => {
+
+        return (
+          <li className="post-list" key={ `${post.id}` }>
+            <div className="post-list-container">
+              <div className="entire-top-container">
+                <div className="post-top-container">
+                  <div className="post-top-left-container">
+                    <div>
+                      <img className="post-form-image" src={ post.image }></img>
+                    </div>
+                    <div className="post-name-container">
+                      <Link to={ `/users/${post.owner}` } replace><p>{ post.first_name }</p></Link>
+                      <p>{ moment(post.created_at).format("LL").slice(0, 10) }</p>
+                    </div>
+                  </div>
+                  { this.postOwner(post) }
+                </div>
+              </div>
+              <p className="post-list-text">{ post.text }</p>
+            </div>
+          </li>
+        );
+      });
+    }
   }
 
   render() {
