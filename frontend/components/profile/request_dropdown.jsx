8842ca80f0c8859b119546480bc0a855b106b724
Steven <codebusiness2014@gmail.com>
2018-02-28 15:04:48 -0500

    Added a no request for when there aren't any request present

--- frontend/components/profile/request_dropdown.jsx
+++ frontend/components/profile/request_dropdown.jsx
@@ -18,49 +18,43 @@ class RequestDropDown extends Component {
     });
   }
 
-  temporary() {
-    let ids = this.props.user.requests.filter( id => id !== this.props.currentUser.id);
-    menu = ids.map( id => {
-      return (
-        <div key={id}>
-          <ul className="request-dropdown">
-            <div className="request-options">
-              <li className="selection-option"><button onClick={ () => this.props.updateFriendship(id) }>Accept</button></li>
-            </div>
-          </ul>
-        </div>
-      );
-    });
-
-  }
-
   render() {
       let list_requests;
 
       if(this.state.menuActive) {
-        let requestors = Object.values(this.props.requests);
+        if (this.props.requests) {
+          let requestors = Object.values(this.props.requests);
 
-        list_requests = requestors.map( (user, idx ) => {
-          return (
-            <div className="request_list_items" key={ user.id }>
-              <div className="request_list_left">
-                <img className="requestor-image" src={ user.image }></img>
-                <span className="requestor-name"><Link to={`/users/${user.id}`}>{ user.first_name } { user.last_name }</Link></span>
+          list_requests = requestors.map( (user, idx ) => {
+            return (
+              <div className="request_list_items" key={ user.id }>
+                <div className="request_list_left">
+                  <img className="requestor-image" src={ user.image }></img>
+                  <span className="requestor-name"><Link to={`/users/${user.id}`}>{ user.first_name } { user.last_name }</Link></span>
+                </div>
+                <div className="request-options">
+                  <div className="selection-option"><button onClick={ () => this.props.updateFriendship(user.id) }>Accept</button></div>
+                </div>
               </div>
-              <div className="request-options">
-                <div className="selection-option"><button onClick={ () => this.props.updateFriendship(id) }>Accept</button></div>
-              </div>
+            );
+          });
+
+          list_requests = <div>
+            <div>
+              <div className="arrow-up"></div>
+              <p className="friend-request-text">Friend Requests</p>
+              { list_requests }
             </div>
-          );
-        });
-
-        list_requests = <div>
-          <div>
-            <div className="arrow-up"></div>
-            <p className="friend-request-text">Friend Requests</p>
-            { list_requests }
-          </div>
-        </div>;
+          </div>;
+        } else {
+          list_requests = <div>
+            <div>
+              <div className="arrow-up"></div>
+              <p className="friend-request-text">Friend Requests</p>
+              <p>No Friend Request</p>
+            </div>
+          </div>;
+        }
 
 
       } else {
