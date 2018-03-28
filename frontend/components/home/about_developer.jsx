1f2abaa6e960e870cbdc2b87a330bc03009986eb
Steven <codebusiness2014@gmail.com>
2018-03-28 10:24:07 -0400

    Needed to add // to my a tags in order to redirect to an external link

--- frontend/components/home/about_developer.jsx
+++ frontend/components/home/about_developer.jsx
@@ -1,9 +1,14 @@
 import React from 'react';
+import { Link, Redirect } from 'react-router-dom';
 
 const AboutDeveloper = () => {
   return(
     <div>
-      <p>Hello</p>
+      <div className="about-developer-container">
+        <a href="//linkedin.com/in/steven-ossorio/"><i className="fa fa-linkedin"></i></a>
+        <a href='//github.com/steven-ossorio/SocialBook'><i className="fa fa-github"></i></a>
+        <a href='//www.stevenossorio.com'><i className="fa fa-globe"></i></a>
+      </div>
     </div>
   );
 };
