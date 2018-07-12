56cbeaef27d4ac2cf8015cdb9e52aee0863a638e
Steven <codebusiness2014@gmail.com>
2018-07-12 15:18:04 -0400

    Post comments to appear 3 at a time

--- frontend/components/comment/comment_list.jsx
+++ frontend/components/comment/comment_list.jsx
@@ -1,19 +1,24 @@
-import React, { Component } from 'react';
-import moment from 'moment';
-import { Link } from 'react-router-dom';
-import { RingLoader } from 'react-spinners';
-
+import React, { Component } from "react";
+import moment from "moment";
+import { Link } from "react-router-dom";
+import { RingLoader } from "react-spinners";
 
 class CommentList extends Component {
   constructor(props) {
     super(props);
+
     this.state = {
+      startIndex: this.props.comments[this.props.postId].length,
+      endIndex: this.props.comments[this.props.postId].length
     };
     this.renderComments = this.renderComments.bind(this);
   }
 
   shouldComponentUpdate(nextProps, nextState) {
-    if (nextProps.comments[this.props.postId].length !== this.props.comments[this.props.postId].length) {
+    if (
+      nextProps.comments[this.props.postId].length !==
+      this.props.comments[this.props.postId].length
+    ) {
       return true;
     }
 
@@ -21,35 +26,53 @@ class CommentList extends Component {
   }
 
   renderComments() {
-    if (this.props.comments === null && this.props.comments.length > 0) {
-      return (
-        <div className="loading-spin">
-
-        </div>
+    let loadMoreComments =
+      this.props.comments[this.props.postId].length > 3 ? (
+        <p>Load More Comments</p>
+      ) : (
+        ""
       );
-    } else {
-      return this.props.comments[this.props.postId].map( (comment, idx) => {
-        return (
-          <li className="post-list" key={ `${comment.id}` }>
+    let commentList = this.props.comments[this.props.postId].slice(
+      this.state.startIndex - 3,
+      this.state.endIndex
+    );
+
+    commentList = commentList.map((comment, idx) => {
+      return (
+        <div>
+          <li className="post-list" key={`${comment.id}`}>
             <div className="comment-container">
-              <img src={comment.user.image}></img>
+              <img src={comment.user.image} />
               <div className="comment-content">
-                  <Link className="comment-owner-name" to={`/users/${comment.user.id}`} replace>{comment.user.first_name}</Link>
-                  <span>{comment.text}</span>
+                <Link
+                  className="comment-owner-name"
+                  to={`/users/${comment.user.id}`}
+                  replace
+                >
+                  {comment.user.first_name}
+                </Link>
+                <span>{comment.text}</span>
               </div>
             </div>
           </li>
-        );
-      });
+        </div>
+      );
+    });
+
+    if (this.props.comments === null && this.props.comments.length > 0) {
+      return <div className="loading-spin" />;
+    } else {
+      return (
+        <div>
+          {loadMoreComments}
+          {commentList}
+        </div>
+      );
     }
   }
 
   render() {
-    return (
-      <div className="list-of-post">
-        { this.renderComments() }
-      </div>
-    );
+    return <div className="list-of-post">{this.renderComments()}</div>;
   }
 }
 
--- frontend/components/post/post_index.jsx
+++ frontend/components/post/post_index.jsx
@@ -1,12 +1,11 @@
-import React, { Component } from 'react';
-import DropDown from './dropdown';
-import moment from 'moment';
-import { Link } from 'react-router-dom';
-import { RingLoader } from 'react-spinners';
-import CommentForm from '../comment/comment_form';
-import CommentList from '../comment/comment_list';
-import CommentContainer from '../comment/comment_container';
-
+import React, { Component } from "react";
+import DropDown from "./dropdown";
+import moment from "moment";
+import { Link } from "react-router-dom";
+import { RingLoader } from "react-spinners";
+import CommentForm from "../comment/comment_form";
+import CommentList from "../comment/comment_list";
+import CommentContainer from "../comment/comment_container";
 
 class PostIndex extends Component {
   constructor(props) {
@@ -19,7 +18,9 @@ class PostIndex extends Component {
   }
 
   componentWillReceiveProps(nextProps, nextState) {
-    if (Object.keys(this.props.posts).length < Object.keys(nextProps.posts).length) {
+    if (
+      Object.keys(this.props.posts).length < Object.keys(nextProps.posts).length
+    ) {
       this.props.fetchUser(nextProps.match.params.userId);
     }
   }
@@ -28,7 +29,7 @@ class PostIndex extends Component {
     if (post.owner === this.props.currentUser.id) {
       return (
         <div>
-          <DropDown deletePost={ this.props.deletePost } post={ post }/>
+          <DropDown deletePost={this.props.deletePost} post={post} />
         </div>
       );
     }
@@ -38,37 +39,50 @@ class PostIndex extends Component {
     if (this.props.profilePostsId === null) {
       return (
         <div className="loading-spin">
-          <RingLoader size={100} color={'#0000FF'} />
+          <RingLoader size={100} color={"#0000FF"} />
         </div>
       );
     } else {
-      let posts = this.props.profilePostsId.sort( (a, b) => a - b).reverse().map( id => {
-        return this.props.posts[id];
-      });
+      let posts = this.props.profilePostsId
+        .sort((a, b) => a - b)
+        .reverse()
+        .map(id => {
+          return this.props.posts[id];
+        });
 
-      return posts.map( (post, idx) => {
+      return posts.map((post, idx) => {
         return (
-          <li className="post-list" key={ `${post.id}` }>
+          <li className="post-list" key={`${post.id}`}>
             <div className="post-list-container">
               <div className="entire-top-container">
                 <div className="post-top-container">
                   <div className="post-top-left-container">
                     <div>
-                      <img className="post-form-image" src={ post.image }></img>
+                      <img className="post-form-image" src={post.image} />
                     </div>
                     <div className="post-name-container">
-                      <Link to={ `/users/${post.owner}` } replace><p>{ post.first_name }</p></Link>
-                      <p>{ moment(post.created_at).format("LL").slice(0, 10) }</p>
+                      <Link to={`/users/${post.owner}`} replace>
+                        <p>{post.first_name}</p>
+                      </Link>
+                      <p>
+                        {moment(post.created_at)
+                          .format("LL")
+                          .slice(0, 10)}
+                      </p>
                     </div>
                   </div>
-                  { this.postOwner(post) }
+                  {this.postOwner(post)}
                 </div>
               </div>
-              <p className="post-list-text">{ post.text }</p>
+              <p className="post-list-text">{post.text}</p>
               <div>
                 <CommentContainer postId={post.id} />
               </div>
-              <CommentForm user={ this.props.user } postId={ post.id } createComment={ this.props.createComment }  />
+              <CommentForm
+                user={this.props.user}
+                postId={post.id}
+                createComment={this.props.createComment}
+              />
             </div>
           </li>
         );
@@ -77,11 +91,7 @@ class PostIndex extends Component {
   }
 
   render() {
-    return (
-      <div className="list-of-post">
-        { this.renderPosts() }
-      </div>
-    );
+    return <div className="list-of-post">{this.renderPosts()}</div>;
   }
 }
 
