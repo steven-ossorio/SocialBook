2c03a34f16f76815ec3f59231b8da3829da1f27e
Steven <codebusiness2014@gmail.com>
2018-07-19 18:12:48 -0400

    Adding styling for friends

--- frontend/components/profile/friend_list_item.jsx
+++ frontend/components/profile/friend_list_item.jsx
@@ -5,20 +5,25 @@ export default props => {
     let friends = Object.keys(props.friends).map(friendId => {
       let friend = props.friends[friendId];
       return (
-        <div className="friend-list-container">
-          <div className="friend-list-description">
-            <img className="friend-list-image" src={friend.image} alt="" />
-            <span className="friend-list-name">
+        <div className="friends-list-item">
+          <div className="friends-list-description">
+            <img className="friends-list-image" src={friend.image} alt="" />
+            <span className="friends-list-name">
               {friend.first_name} {friend.last_name}
             </span>
           </div>
-          <div className="friend-list-button">
+          <div className="friends-list-button">
             <button>WE ARE FRIENDS</button>
           </div>
         </div>
       );
     });
-    return <div>IT LOADED {friends}</div>;
+    return (
+      <div className="friends-container">
+        <div className="friends-list-header">Friends</div>
+        <div className="friends-list-container">{friends}</div>
+      </div>
+    );
   } else {
     return <div>DID NOT LOAD</div>;
   }
